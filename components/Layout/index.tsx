af7db585f8c7d23a04deb260671ae787ae014176
Jeff <jeffrey.manful@gmail.com>
2021-01-29 20:10:48 +0000

    Add global theme and css

---
+++ components/Layout/index.tsx
@@ -0,0 +1,4 @@
+const Layout = ({ children }) => <>{children}</>
+
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+const Landing = () => {
+  return (
+    <div>
+      <section className="section--intro flex-col">
+        <h1>Verlay002</h1>
+        <p>Follow the signs</p>
+      </section>
+
+      <section>
+        <p>Listen to your gut</p>
+      </section>
+
+      <section className="section--third">
+        <h3>Why the hell not</h3>
+      </section>
+    </div>
+  )
+}
+
+export default Landing;
